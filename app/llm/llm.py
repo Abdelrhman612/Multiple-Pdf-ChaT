3ec7eb3c0c264c28f48c4ab506397626f12bd38e
Abdelrahman <abdelrahman.ayman.tech@gmail.com>
2026-04-30 17:35:51 +0300

    refactor: switch from streaming to blocking response generation with loading indicator

--- app/llm/llm.py
+++ app/llm/llm.py
@@ -3,4 +3,4 @@ from langchain_ollama import OllamaLLM
 llm = OllamaLLM(model="llama3.2" , temperature=0)
 
 def generate_answer(prompt):
-    return llm.stream(prompt)
+    return llm.invoke(prompt)
